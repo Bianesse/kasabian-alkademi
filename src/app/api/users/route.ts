4dc8955c2cfcde37723d68ff529e7b1660e41549
Bianesse <ibrahimkasabian@gmail.com>
2025-07-17 10:13:30 +0700

    feat: prisma basic

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,49 +1,52 @@
+/* import { prisma } from '@/lib/prisma'; */
+
 const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Doe" },
-    { id: 3, name: "Bob Smith" },
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
+  { id: 3, name: "Bob Smith" },
 ];
 
 export async function GET() {
-    return new Response(JSON.stringify(users), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+  /* const user = await prisma.user.findMany();  */
+  return new Response(JSON.stringify(users), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export async function POST(request: Request) {
-    const body = await request.json();
+  const body = await request.json();
 
-    users.push(body);
-    return new Response(JSON.stringify(body), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+  users.push(body);
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export async function DELETE(request: Request) {
-    const { id } = await request.json();
-  
-    const index = users.findIndex((user) => user.id === id);
-  
-    if (index !== -1) {
-      const deletedUser = users.splice(index, 1)[0];
-  
-      return new Response(JSON.stringify(deletedUser), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-  
-    return new Response(JSON.stringify({ error: "User not found" }), {
-      status: 404,
+  const { id } = await request.json();
+
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    const deletedUser = users.splice(index, 1)[0];
+
+    return new Response(JSON.stringify(deletedUser), {
+      status: 200,
       headers: { "Content-Type": "application/json" },
     });
   }
+
+  return new Response(JSON.stringify({ error: "User not found" }), {
+    status: 404,
+    headers: { "Content-Type": "application/json" },
+  });
+}
 
 export async function PUT(request: Request) {
   const { id, name } = await request.json();
